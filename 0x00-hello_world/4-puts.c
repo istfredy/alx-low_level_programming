ff59c240e7b38130f7f00e18bb60595c288cebee
MrDeveloper01010 <taboutsafredy@gmail.com>
2023-02-16 18:28:03 -0800

    Hamza & Damso - Nocif

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
 * main - entry point of the program
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 /* Code here */
 return (0);
 }
