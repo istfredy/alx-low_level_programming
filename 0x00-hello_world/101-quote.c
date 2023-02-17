3dd0b2c685c553f6b0b637fe7206e0ecb51dd50a
MrDeveloper01010 <taboutsafredy@gmail.com>
2023-02-16 16:38:42 -0800

    MEVTR-DAMSO-2023-FIN-OF-QALFTOUR

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,2 @@
-#include <unistd.h>  int main(void) {     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";     write(2, message, 42);     return 1; }
+#include <unistd.h>
+int main(void) {     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";     write(2, message, 42);     return 1; }
