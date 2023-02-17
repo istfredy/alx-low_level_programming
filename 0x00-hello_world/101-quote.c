d51a66f4e66915d239d4c37afe93bd1dcb1d584d
MrDeveloper01010 <taboutsafredy@gmail.com>
2023-02-16 18:24:45 -0800

    Hamza & Damso - Nocif

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
-/**  
- * main - Entry point  
- *   
- * Return: Always 1 (Error code)  
- */ 
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error code)
+ */
+int main(void)
 {
-  const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-     
-  write(2, message, 59);  
-  return (1); 
+/* Code here */
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#include <stdio.h>  
-int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }
+#include <stdio.h>
+/**
+* main - entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+/* Code here */
+return (0);
+}
