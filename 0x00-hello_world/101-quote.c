2b0c463e166649c23f8bcfe498f657e3e186d9b2
MrDeveloper01010 <taboutsafredy@gmail.com>
2023-02-16 17:04:20 -0800

    FOR A FIRST BLACK I AM SUPPOSE TO BAD

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,15 @@
-#include <unistd.h>
-int main(void) {     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";     write(2, message, 59);     return 1; }
+#include <unistd.h>  
+/**  
+ * main - Entry point  
+ *  
+ * Description: Prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19  
+ *          to the standard error  
+ * Return: 1 (Error code)  
+ */ 
+int main(void) 
+{
+  const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+     
+  write(2, message, 59);  
+  return (1); 
+}
