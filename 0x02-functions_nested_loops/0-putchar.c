4853f9bdd8cf01f45268edb025cf01a02b60639b
MrDeveloper01010 <taboutsafredy@gmail.com>
2023-02-22 16:08:27 -0800

    0-putchar.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,10 @@
  *
  * Return: 0 on success and none on faillure
  */
-int main(void)
+main(void)
 {
 /* My Code C..... */
-_putchar(95);/*because 95 is a value of "_" in ASCII */
+_putchar("_");/*because 95 is a value of "_" in ASCII */
 _putchar("p");
 _putchar("u");
 _putchar("t");
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  * THis is like creating my own library with declared functions
  */
 int main(void);
-int _putchar(int);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
