6404b85d954296dac1cb2f031c2899135b985de1
Mr. Developer <taboutsafredy@gmail.com>
2023-03-03 02:47:01 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,22 +6,23 @@
  *
  * Return: pointer to the encoded string.
  */
+
 char *rot13(char *s)
 {
-/*My code Here !!!!!*/
 int i, j;
 char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char boolean;
+
 for (i = 0; s[i] != '\0'; i++)
 {
 boolean = 0;
 for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
-{	
-if (s[i] == alpha[j])	
-{		
-s[i] = r[j];		
-boolean = 1;	
+{
+if (s[i] == alpha[j])
+{
+s[i] = r[j];
+boolean = 1;
 }
 }
 }
