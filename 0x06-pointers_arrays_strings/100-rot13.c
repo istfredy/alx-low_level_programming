c0dc8f388df39ebc3d53e6b8013c9dc8f46837ce
Mr. Developer <taboutsafredy@gmail.com>
2023-03-03 03:07:03 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,22 +9,22 @@
 
 char *rot13(char *s)
 {
-int i, j;
-char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char boolean;
+	int i, j;
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-boolean = 0;
-for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
-{
-if (s[i] == alpha[j])
-{
-s[i] = r[j];
-boolean = 1;
-}
-}
-}
-return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = r[j];
+				boolean = 1;
+			}
+		}
+	}
+	return (s);
 }
