fe759a7935193555065edec6cf22c845a40e0829
th3fr3dy <taboutsafredy@gmail.com>
2023-09-12 20:33:44 -0700

    Fix Variable Name Error of 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,16 +3,16 @@
 /**
  * flip_clip - counts the number of bits to change
  * to get from one number to anothe
- * @a: first number
- * @b: second number
+ * @n: first number
+ * @m: second number
  *
  * Return: number of bits to change
  */
-unsigned int flip_bits(unsigned long int a, unsigned long int b)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, counter = 0;
 	unsigned long int current;
-	unsigned long int exclusive = a ^ b;
+	unsigned long int exclusive = n ^ m;
 
 	for (i = 63; i >= 0; i--)
 	{
