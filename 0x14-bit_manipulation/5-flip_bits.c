4aa636f7c815bc48189d0a529fa09c1577daaffc
Fredy ⚙️ <taboutsafredy@gmail.com>
2023-09-21 01:24:48 +0000

    Fix Requirment Error of 5-flip_bits.c file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * flip_clip - counts the number of bits to change
- *		to get from one number to another.
- * @n: first number.
- * @m: second number to flip n to.
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
  *
  * Return: number of bits to flip to get from n to m.
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int bts, check;
-	unsigned int i, count;
+	unsigned long int btw, check;
+	unsigned int num, i;
 
 	check = 1;
-	bts = n ^ m;
-	count = 0;
+	btw = n ^ m;
+	num = 0;
 
 	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
-		if (check == (bts & check))
-			count++;
+		if (check == (btw & check))
+			num++;
 		check <<= 1;
 	}
-
-	return (count);
+	return (num);
 }
-
